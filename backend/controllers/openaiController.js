417d232ca2740d67200e720e7cd815d29608d3b5
Robbie Prokop <robbieprokop@gmail.com>
2022-12-20 19:06:56 -0700

    create res for create image

--- backend/controllers/openaiController.js
+++ backend/controllers/openaiController.js
@@ -1,7 +1,25 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 const generateImage = async (req, res) => {
-  res.status(200).json({
-    success: true,
-  });
+  try {
+    const res = await openai.createImage({
+      prompt: "Russian civilians slugging away in a cold blizzard",
+      n: 1,
+      size: "512x512",
+    });
+
+    const imageUrl = res.data.data[0].url;
+
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {}
 };
 
 module.exports = { generateImage };
