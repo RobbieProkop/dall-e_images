d0b497eab502dabfc629eceac4773252dc898d66
Robbie Prokop <robbieprokop@gmail.com>
2022-12-20 18:52:45 -0700

    create post request

--- backend/routes/openAIRoutes.js
+++ backend/routes/openAIRoutes.js
@@ -1,1 +1,10 @@
-// hi
+const express = require("express");
+const router = express.Router();
+
+router.post("/generateimage", (req, res) => {
+  res.status(200).json({
+    success: true,
+  });
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use("/openai", require("./routes/openAIRoutes"));
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
