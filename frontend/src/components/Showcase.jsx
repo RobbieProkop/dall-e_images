a98cd65bdea71e4d840e4da215350fab5323eeec
Robbie Prokop <robbieprokop@gmail.com>
2022-12-20 21:54:23 -0700

    begin working on the shorcase

--- frontend/src/components/Showcase.jsx
+++ frontend/src/components/Showcase.jsx
@@ -1,6 +1,12 @@
 import "../styles/showcase.scss";
 
 const Showcase = () => {
-  return <section>Showcase</section>;
+  return (
+    <section>
+      <form id="image-form">
+        <h1>What kind of Image do you want?</h1>
+      </form>
+    </section>
+  );
 };
 export default Showcase;
