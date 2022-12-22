955f7c3904d1b12194b63af5075c79cfcf89ff59
Robbie Prokop <robbieprokop@gmail.com>
2022-12-21 20:10:06 -0700

    complete showcase html

--- frontend/src/components/Showcase.jsx
+++ frontend/src/components/Showcase.jsx
@@ -5,6 +5,21 @@ const Showcase = () => {
     <section>
       <form id="image-form">
         <h1>What kind of Image do you want?</h1>
+        <div className="form-control">
+          <input type="text" id="prompt" placeholder="Describe an image" />
+        </div>
+
+        {/* size */}
+        <div className="form-control">
+          <select name="size" id="size">
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
+        </div>
+        <button type="submit" className="btn">
+          Generate
+        </button>
       </form>
     </section>
   );
