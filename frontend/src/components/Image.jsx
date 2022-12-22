1b909a9c23263af79ec0e9d3a3e4c5663efffe69
Robbie Prokop <robbieprokop@gmail.com>
2022-12-21 20:14:16 -0700

    create image component

---
+++ frontend/src/components/Image.jsx
@@ -0,0 +1,11 @@
+const Image = () => {
+  return (
+    <section className="image">
+      <div className="image-container">
+        <h2 className="msg"></h2>
+        <img src="" alt="" id="image" />
+      </div>
+    </section>
+  );
+};
+export default Image;
